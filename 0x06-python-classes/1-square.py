041b5175fc1938771a8ebb36a7359ccf53dd6c1d
Seunelvis1 <adeniranoluwaseunelvis@gmail.com>
2023-08-29 21:02:05 -0700

    working well I guess

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''DEfine a class Square'''
+
+class Square:
+    '''Represent a square'''
+
+    def __init__(self,size):
+        '''initialize a new Square
+
+        Args:
+            Size (init): The size of the new square.
+            '''
+            self.__size = size
